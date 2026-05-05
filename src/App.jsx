a5d75c8bc984386e5be71f111108c49f6f947990
dpadillla_22 <dpadilla_22@alu.uabcs.mx>
2026-05-04 20:04:20 -0700

    initial structure React + Vite + MUI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <>
+      <h1>Mi proyecto</h1>
+      <Button variant="contained">Botón pro</Button>
+    </>
+  );
+}
+export default App
